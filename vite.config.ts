f855d7d6a902fb8f66c1d08f9f07c53a2a31c570
andersseen <andriipap01@gmail.com>
2026-01-17 21:51:05 +0100

    Add Astro

--- vite.config.ts
+++ vite.config.ts
@@ -1,5 +1,6 @@
 import { defineConfig } from "vite";
 import tailwindcss from "@tailwindcss/vite";
+import dts from "vite-plugin-dts";
 
 export default defineConfig(({ mode }) => {
   if (mode === "app") {
@@ -12,12 +13,13 @@ export default defineConfig(({ mode }) => {
   }
 
   return {
-    plugins: [tailwindcss()],
+    plugins: [tailwindcss(), dts({ rollupTypes: true })],
     build: {
       lib: {
         entry: "src/index.ts",
         formats: ["es"],
       },
+      outDir: "dist-lib",
       rollupOptions: {
         external: [/^lit/],
       },
